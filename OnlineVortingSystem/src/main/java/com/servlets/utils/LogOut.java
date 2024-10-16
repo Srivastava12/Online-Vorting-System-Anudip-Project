c2154770c0de04c38e60848729c1f2c14acf4372
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-10-16 12:20:46 +0530

    Update LogOut.java

--- OnlineVortingSystem/src/main/java/com/servlets/utils/LogOut.java
+++ OnlineVortingSystem/src/main/java/com/servlets/utils/LogOut.java
@@ -25,10 +25,15 @@ public class LogOut extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session=request.getSession(true);
-		session.invalidate();
+		// Retrieve or create a new HTTP session for the user
+		HttpSession session = request.getSession(true); // 'true' means it will create a new session if one doesn't exist
+
+		// Invalidate the current session, logging the user out and clearing all session attributes
+		session.invalidate(); // This method invalidates the session, removing any stored data
+
+		// Forward the request and response to the "index.html" page after logging the user out
 		RequestDispatcher rd = request.getRequestDispatcher("index.html");
-		rd.forward(request, response);
+		rd.forward(request, response); // Forward the user to the homepage (index.html)
 	}
 
 	/**
