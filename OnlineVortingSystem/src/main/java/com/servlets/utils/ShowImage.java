9fa61e2fb9f514599272d151a575d27d5231a7bb
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-10-16 12:25:07 +0530

    Update ShowImage.java

--- OnlineVortingSystem/src/main/java/com/servlets/utils/ShowImage.java
+++ OnlineVortingSystem/src/main/java/com/servlets/utils/ShowImage.java
@@ -29,25 +29,42 @@ public class ShowImage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			
+			// Set the content type of the response to "image/gif" as the output is an image
 			response.setContentType("image/gif");
-			HttpSession session=request.getSession(true);
-			String cnadidateNumber = (String) session.getAttribute("number");
-			
-			byte[] image=null;
-			ServletOutputStream sos=null;
-			
-		    String sqlQuery ="select images from candidateimage where candidateNumber=?";
-			
+
+			// Get the current HTTP session or create a new one if it doesn't exist
+			HttpSession session = request.getSession(true);
+
+			// Retrieve the candidate's number stored in the session (from previous actions)
+			String candidateNumber = (String) session.getAttribute("number");
+
+			byte[] image = null;  // Byte array to hold the image data from the database
+			ServletOutputStream sos = null;  // Output stream to write the image back to the client (browser)
+
+			// SQL query to fetch the image associated with the candidate's number from the "candidateimage" table
+			String sqlQuery = "select images from candidateimage where candidateNumber=?";
+
+			// Prepare the SQL query
 			PreparedStatement ps = conn.prepareStatement(sqlQuery);
-    		ps.setString(1,cnadidateNumber);
-    		
-    		ResultSet candidateImage=ps.executeQuery();
-    		
-    		while(candidateImage.next()) {
-               image = candidateImage.getBytes("images");
-               sos = response.getOutputStream();
-               sos.write(image);
-    		}
+
+			// Set the candidate's number as the query parameter
+			ps.setString(1, candidateNumber);
+
+			// Execute the query and get the result set containing the image
+			ResultSet candidateImage = ps.executeQuery();
+
+			// Loop through the result set to retrieve the image (if available)
+			while (candidateImage.next()) {
+   			 // Get the image data as a byte array from the "images" column
+    			image = candidateImage.getBytes("images");
+
+   			 // Get the output stream to write the image to the HTTP response
+    			sos = response.getOutputStream();
+
+    			// Write the image data to the output stream, sending it to the client
+    			sos.write(image);
+			}
+
 		}catch(Exception e) {
 			e.getStackTrace();
 		}
